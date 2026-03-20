c199406cc8018231c917c3b01cc8a0c97e989314
duybaro2812 <duybaro2812@gmail.com>
2026-03-20 20:54:04 +0700

    Product-manager

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const route = require("./routes/index.route")
+const app = express();
+const port = 3000;
+
+
+app.set("views", "./views");
+app.set("view engine", "pug")
+
+// app.get('/', (req, res) => {
+//   res.render("client/pages/home/index");
+// })
+
+// app.get('/products', (req, res) => {
+//   res.render("client/pages/products/index");
+// })
+route(app);
+
+app.listen(port, () => {
+  console.log(`App listening on ${port} : http://localhost:3000/ `)
+})
