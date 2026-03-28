171a289c457bd365ea0c51fbd1455f29356f61fc
duybaro2812 <duybaro2812@gmail.com>
2026-03-28 23:16:42 +0700

    Connect Database MongoDB

---
+++ models/product.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+const productSchema = new mongoose.Schema({ 
+    title : String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: Boolean
+});
